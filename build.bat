fa895158b56cf445d2786e46ff71a45b8e1a5e16
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-03-16 15:27:39 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,3 +4,4 @@ cd "%cd%\launcher-fancy\build\libs"
 "%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher-fancy-all.jar.pack launcher-fancy-*.*.*-all.jar
 cd "..\..\..\launcher\build\libs"
 "%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher-all.jar.pack launcher-*.*.*-all.jar
+pause
