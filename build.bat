7d0505abd1c9d837d1a04c238e8e3dd99504b0d7
Jacob Schenke <jacobdschenke@gmail.com>
2019-03-16 15:50:03 -0500

    Remove everything in master2 branch except for README.md

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call gradlew clean build
-cd "%cd%\launcher-fancy\build\libs"
-"%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher-fancy-all.jar.pack launcher-fancy-*.*.*-all.jar
-cd "..\..\..\launcher\build\libs"
-"%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher-all.jar.pack launcher-*.*.*-all.jar
-pause
