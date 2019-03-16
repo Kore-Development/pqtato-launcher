b6edf3c55368f70a8f72fb3dfc2e05306f7345b9
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-03-16 18:41:31 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -8,4 +8,5 @@ ren "launcher-fancy-*.*.*-all.jar" "launcher-fancy.jar"
 ren "launcher-bootstrap-*.*.*-all.jar" "launcher-bootstrap.jar"
 "%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher-fancy.jar.pack launcher-fancy.jar
 "%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher.jar.pack launcher.jar
+del "launcher-builder-*.*.*-all.jar"
 pause
