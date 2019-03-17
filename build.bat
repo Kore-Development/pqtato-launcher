c8a7d10b3dea41b8da35288966647ab2e4851f66
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-03-16 19:13:45 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,10 +2,10 @@
 call gradlew clean build
 cd "%cd%\launcher-fancy\build\libs"
 ren "launcher-fancy-*.*.*-all.jar" "launcher-fancy-all.jar"
-"%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher-fancy-all.jar.pack launcher-fancy-all.jar
+pack200 --no-gzip launcher-fancy-all.jar.pack launcher-fancy-all.jar
 cd "..\..\..\launcher\build\libs"
 ren "launcher-*.*.*-all.jar" "launcher-all.jar"
-"%JAVA_HOME%\bin\pack200.exe" --no-gzip launcher-all.jar.pack launcher-all.jar
+pack200 --no-gzip launcher-all.jar.pack launcher-all.jar
 cd "..\..\..\launcher-bootstrap\build\libs"
 ren "launcher-bootstrap-*.*.*-all.jar" "launcher-bootstrap-all.jar"
 pause
